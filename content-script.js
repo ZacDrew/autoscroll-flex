5a222d2e4eb55381668b929e6cd5f0499e109222
ZacDrew <noreply@example.com>
2026-01-26 23:24:29 +1000

    add scroll function and listeners to update scroll settings

--- content-script.js
+++ content-script.js
@@ -1,11 +1,59 @@
 
-browser.storage.onChanged.addListener((changes, area) => {
-    if (area == 'local' && 
-        ('distance' in changes || 'delay' in changes)
-    ) {
-        update(changes.distance.newValue);
-        // console.log('changes registered');
+let scrollInterval = null, yPos = 0, distance = 10, delay = 1000;
+
+// TODO:
+// - grab distance and delay from storage to set initial values
+// - update yPos after scrolling manually so that stop starting
+//   doesnt jump around the page
+// - stop scroll when not in tab (but still scroll when unfocused)
+
+if (typeof window !== 'undefined') {
+    yPos = window.scrollY;
+}
+
+function startScroll() {
+
+    scrollInterval = setInterval(() => {
+        yPos += distance;
+        window.scroll(
+            {
+            top: yPos,
+            behavior: 'smooth'
+            }
+        );
+    }, delay);
+}
+
+browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.command === 'start') {
+        if (scrollInterval) return;
+        startScroll();
+        return;
     }
+    if (msg.command === 'stop') {
+        clearInterval(scrollInterval);
+        scrollInterval = null;
+        return;
+    }
+    if (msg.command === 'status') {
+        sendResponse({ running: !!scrollInterval });
+        return true;
+    }
+
 });
 
-// function update()
+
+browser.storage.onChanged.addListener((changes, area) => {
+    if (area == 'local') {
+        if ('distance' in changes) {
+            distance = changes.distance.newValue;
+            console.log('distance updated');
+
+        }
+        if ('delay' in changes) {
+            // Fetch and convert from sec to millisec
+            delay = changes.delay.newValue * 1000;
+            console.log('delay updated');
+        }
+    }
+});
