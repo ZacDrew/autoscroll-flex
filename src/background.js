4b8b2bdb9760c4f4b97c7d544511c33024560089
ZacDrew <ZacDrew@users.noreply.github.com>
2026-02-05 17:53:17 +1000

    feat: change background to set all defaults, not just disabled sites

--- src/background.js
+++ src/background.js
@@ -2,13 +2,19 @@ import * as C from './constants/storage.js';
 
 browser.runtime.onInstalled.addListener(async () => {
 
-    // Set default disabled sites if not already set
-    const result = await browser.storage.local.get(
-        C.STORAGE_KEY.DISABLED_SITES
+    const existing = await browser.storage.local.get(
+        Object.values(C.STORAGE_KEY)
     );
-    if (!result[C.STORAGE_KEY.DISABLED_SITES]) {
-        await browser.storage.local.set({ 
-            [C.STORAGE_KEY.DISABLED_SITES]: C.DEFAULT.DISABLED_SITES
-        });
+
+    const defaultsToSet = {};
+
+    for (const [keyName, storageKey] of Object.entries(C.STORAGE_KEY)) {
+        if (existing[storageKey] === undefined) {
+            defaultsToSet[storageKey] = C.DEFAULT[keyName];
+        }
+    }
+
+    if (Object.keys(defaultsToSet).length) {
+        await browser.storage.local.set(defaultsToSet);
     }
 })
