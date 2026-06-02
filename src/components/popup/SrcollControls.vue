02c0ce4a486d105e779e0931bd0510a2084dedec
ZacDrew <ZacDrew@users.noreply.github.com>
2026-06-03 01:24:50 +1000

    wip: make the scroll controll button on the ui

---
+++ src/components/popup/SrcollControls.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useSettings } from '@/composables/useSettings';
+import Button from '../ui/button/Button.vue';
+import { PhCaretDown, PhCaretUp } from '@phosphor-icons/vue';
+import Toggle from '../ui/toggle/Toggle.vue';
+
+</script>
+
+<template>
+
+<div class="absolute bottom-0 left-1/2 -translate-x-1/2 z-9999">
+
+  <div class="flex justify-center p-0 rounded-t-full bg-card outline">
+
+    <div class="w-24 h-24 mx-4 mt-2 mb-2 p-0 rounded-full flex flex-col 
+      items-start justify-center bg-card outline-2">
+
+      <Toggle variant="default"
+        class="group relative w-24 h-12 rounded-t-full [&_svg]:size-auto p-0
+          data-[state=on]:bg-background hover:bg-background"
+      >
+        <PhCaretUp :size="45" weight="bold"
+          class="self-center bg-accen/5 absolute top-1.5 group-data-[state=on]:text-primary"
+        />
+      </Toggle>
+      <Toggle variant="default"
+        class="group relative w-24 h-12 rounded-b-full [&_svg]:size-auto p-0
+          data-[state=on]:bg-background hover:bg-background"
+      >
+        <PhCaretDown :size="45" weight="bold"
+          class="self-center bg-accen/5 absolute bottom-1.5 group-data-[state=on]:text-primary"
+        />
+      </Toggle>
+    
+    </div>
+  </div>
+</div>
+
+</template>
--- src/entrypoints/popup/App.vue
+++ src/entrypoints/popup/App.vue
@@ -4,15 +4,32 @@
 import HelloWorld from '@/components/HelloWorld.vue';
 import HeaderBar from '@/components/popup/HeaderBar.vue';
 import ScrollModes from '@/components/popup/ScrollModes.vue';
+import SrcollControls from '@/components/popup/SrcollControls.vue';
+import Button from '@/components/ui/button/Button.vue';
+
+const scrollContainer = ref<HTMLElement | null>(null)
+
+function onWheel(event: WheelEvent) {
+  scrollContainer.value?.scrollBy({
+    top: event.deltaY,
+  })
+}
 
 </script>
 
 <template>
   <div class="bg-background flex h-full flex-col overflow-hidden min-w-[296px]">
+
     <HeaderBar />
-    <div class="px-3 py-4 flex-1 overflow-y-auto">
+
+    <div class="px-3 py-4 flex-1 overflow-y-auto pb-34"
+      ref="scrollContainer"
+    >
       <ScrollModes />
+
     </div>
+
+    <SrcollControls @wheel="onWheel"/>
     
   </div>
 </template>
